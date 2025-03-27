829bbc6934d66f4cdcff874584ed4d9f05e1422b
0x008080 <caperez.0013@gmail.com>
2025-03-27 12:30:34 -0700

    deploy fix #4

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
-export default {
-    base: '/ArtisanalSoftare/',
-  };
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/', // Use root-relative paths
+});
